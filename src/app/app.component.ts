e966fb861ada3e4048963416ba0eecac3b103738
raven003 <xzardos@web.de>
2021-08-15 11:53:11 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from 'src/api/generated/controllers/Project';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'bootstrapSwaggerGenExample';
+ 
 }
